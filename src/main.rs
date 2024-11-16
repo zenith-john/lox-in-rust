938197a7913c93933ccf2ffaabadd174dee8859c
Zenith John <zenith-john@foxmail.com>
2024-11-16 14:47:07 +0800

    Complete Chapter 4 "Scanning" of "Crafting Interpreters".

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+extern crate lazy_static;
+use std::env;
+use std::process;
+use std::fs::File;
+use std::io;
+use std::io::{Write, BufReader, BufRead, Error};
+
+mod scanner;
+mod token;
+use crate::scanner::scan_tokens;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        println!("Usage: lox [script]");
+        process::exit(0x0040);
+    }
+    else if args.len() == 2 {
+        let _ = run_file(&args[1]);
+    }
+    else {
+        let _ = run_prompt();
+    }
+}
+
+fn run_file(path: &String) -> Result<(), Error> {
+    let input = File::open(path)?;
+    let buffered = BufReader::new(input);
+    let mut had_error = false;
+    let mut l: i32 = 1;
+    for line in buffered.lines() {
+        run(line?, l, &mut had_error);
+        if had_error {
+            process::exit(0x0041)
+        }
+        l = l + 1;
+    }
+    return Ok(());
+}
+
+fn run_prompt() -> Result<(), Error> {
+    let lines = io::stdin().lines();
+    let mut had_error = false;
+    let mut l: i32 = 1;
+    for line in lines {
+        run(line.unwrap(), l, &mut had_error);
+        if had_error {
+            had_error = false;
+        }
+        l = l + 1;
+    }
+    return Ok(());
+}
+
+fn run(source: String, line_number: i32, had_error: &mut bool) {
+    let mut line: i32 = line_number;
+    let tokens = scan_tokens(&source, &mut line);
+    if tokens.len() == 1 {
+        error(line_number, String::from("Invalid input"), had_error);
+    }
+    for token in tokens {
+        println!("{:?}", token);
+    }
+}
+
+pub fn error(line: i32, message: String, had_error: &mut bool) {
+    eprintln!("[Line {}] Error: {}", line, message);
+    *had_error = true
+}
---
+++ src/scanner.rs
@@ -0,0 +1,157 @@
+use crate::token::{Token, TokenType};
+use crate::error;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref keywords: HashMap<String, TokenType> = HashMap::from([
+        ("and".to_string(), TokenType::AND),
+        ("class".to_string(), TokenType::CLASS),
+        ("else".to_string(), TokenType::ELSE),
+        ("false".to_string(), TokenType::FALSE),
+        ("for".to_string(), TokenType::FOR),
+        ("fun".to_string(), TokenType::FUN),
+        ("if".to_string(), TokenType::IF),
+        ("nil".to_string(), TokenType::NIL),
+        ("or".to_string(), TokenType::OR),
+        ("print".to_string(), TokenType::PRINT),
+        ("return".to_string(), TokenType::RETURN),
+        ("super".to_string(), TokenType::SUPER),
+        ("this".to_string(), TokenType::THIS),
+        ("true".to_string(), TokenType::TRUE),
+        ("var".to_string(), TokenType::VAR),
+        ("WHILE".to_string(), TokenType::WHILE),
+    ]);
+}
+
+pub fn scan_tokens(string: &String, line: &mut i32) -> Vec<Token> {
+    let mut start: usize;
+    let mut current: usize = 0;
+    let mut tokens: Vec<Token> = Vec::new();
+    while current < string.len() {
+        start = current;
+        current = scan_token(&string, start, &mut tokens, line);
+    }
+    tokens.push(Token{ttype: TokenType::EOF, lexeme: String::new(), line: *line});
+    return tokens;
+}
+
+fn scan_token(string: &String, pos: usize, tokens: &mut Vec<Token>, line: &mut i32) -> usize {
+    let mut had_error : bool = false;
+    let c:char = string.chars().nth(pos).expect("End of string.");
+    let mut end: usize = pos;
+    match c {
+        '(' => tokens.push(Token{ttype: TokenType::LEFT_PAREN, lexeme: String::from("("), line: *line}),
+        ')' => tokens.push(Token{ttype: TokenType::RIGHT_PAREN, lexeme: String::from(")"), line: *line}),
+        '{' => tokens.push(Token{ttype: TokenType::LEFT_BRACE, lexeme: String::from("{"), line: *line}),
+        '}' => tokens.push(Token{ttype: TokenType::RIGHT_BRACE, lexeme: String::from("}"), line: *line}),
+        ',' => tokens.push(Token{ttype: TokenType::COMMA, lexeme: String::from(","), line: *line}),
+        '.' => tokens.push(Token{ttype: TokenType::DOT, lexeme: String::from("."), line: *line}),
+        '-' => tokens.push(Token{ttype: TokenType::MINUS, lexeme: String::from("-"), line: *line}),
+        '+' => tokens.push(Token{ttype: TokenType::PLUS, lexeme: String::from("+"), line: *line}),
+        ';' => tokens.push(Token{ttype: TokenType::SEMICOLON, lexeme: String::from(";"), line: *line}),
+        '*' => tokens.push(Token{ttype: TokenType::STAR, lexeme: String::from("*"), line: *line}),
+        '!' => {
+            if pos + 1 < string.len() && string.chars().nth(pos + 1).expect("End of string") == '=' {
+                tokens.push(Token{ttype: TokenType::BANG_EQUAL, lexeme: String::from("!="), line: *line});
+                end = pos + 1
+            }
+            else {
+                tokens.push(Token{ttype: TokenType::BANG, lexeme: String::from("!"), line: *line});
+            }
+        },
+        '=' => {
+            if pos + 1 < string.len() && string.chars().nth(pos + 1).expect("End of string") == '=' {
+                tokens.push(Token{ttype: TokenType::EQUAL_EQUAL, lexeme: String::from("=="), line: *line});
+                end = pos + 1
+            }
+            else {
+                tokens.push(Token{ttype: TokenType::EQUAL, lexeme: String::from("="), line: *line});
+            }
+        }
+        '<' => {
+            if pos + 1 < string.len() && string.chars().nth(pos + 1).expect("End of string") == '=' {
+                tokens.push(Token{ttype: TokenType::LESS_EQUAL, lexeme: String::from("<="), line: *line});
+                end = pos + 1
+            }
+            else {
+                tokens.push(Token{ttype: TokenType::LESS, lexeme: String::from("<"), line: *line});
+            }
+        }
+        '>' => {
+            if pos + 1 < string.len() && string.chars().nth(pos + 1).expect("End of string") == '=' {
+                tokens.push(Token{ttype: TokenType::GREATER_EQUAL, lexeme: String::from(">="), line: *line});
+                end = pos + 1
+            }
+            else {
+                tokens.push(Token{ttype: TokenType::GREATER, lexeme: String::from(">"), line: *line});
+            }
+        }
+        '/' => {
+            if pos + 1 < string.len() && string.chars().nth(pos + 1).expect("End of string") == '/' {
+                end = string.len();
+            }
+            else {
+                tokens.push(Token{ttype: TokenType::SLASH, lexeme: String::from("/"), line: *line})
+            }
+        }
+        '"' => {
+            end = pos + 1;
+            while end < string.len() && string.chars().nth(end).expect("End of string") != '"' {
+                end = end + 1
+            }
+            if end == string.len() {
+                error(*line, "Unterminated string.".to_string(), &mut had_error);
+            }
+            else {
+                tokens.push(Token{ttype: TokenType::STRING, lexeme: string[pos+1..end].to_string(), line: *line})
+            }
+        }
+        ' ' | '\t' | '\r' => {},
+        '\n' => *line = *line + 1,
+        '0'..='9' => {
+            end = pos + 1;
+            while end < string.len() && is_digit(string.chars().nth(end).expect("End of string")) {
+                end = end + 1;
+            }
+            if end + 1 < string.len() && string.chars().nth(end).expect("End of string") == '.'
+                && is_digit(string.chars().nth(end + 1).expect("End of string")) {
+                    end = end + 2;
+                    while end < string.len() && is_digit(string.chars().nth(end).expect("End of string")) {
+                        end = end + 1;
+                    }
+                }
+            tokens.push(Token{ttype: TokenType::NUMBER, lexeme: string[pos..end].to_string(), line: *line})
+        },
+        'a'..='z' | 'A'..='Z' => {
+            end = pos + 1;
+            while end < string.len() && is_alpha_numeric(string.chars().nth(end).expect("End of string")) {
+                end = end + 1;
+            }
+            let text = string[pos..end].to_string();
+            let ttype: TokenType = match keywords.get(&text) {
+                Some(i) => i.clone(),
+                None => TokenType::IDENTIFIER
+            };
+            tokens.push(Token{ttype: ttype, lexeme: text, line: *line});
+        }
+        _ =>  {
+            error(*line, "Unexpected character".to_string(), &mut had_error);
+        }
+    }
+    return end + 1;
+}
+
+fn is_digit(c: char) -> bool {
+    match c {
+        '0'..='9' => true,
+        _ => false
+    }
+}
+
+fn is_alpha_numeric(c: char) -> bool {
+    match c {
+        '0' ..= '9' | 'a' ..= 'z' | 'A' ..= 'Z' => true,
+        _ => false
+    }
+}
